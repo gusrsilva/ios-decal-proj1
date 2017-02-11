2d940b6817aca6da2635b6697e7793ae2e1a8d43
Gustavo Silva <gus7silva@berkeley.edu>
2017-02-11 10:24:01 -0800

    update incorrect guesses UI

--- Hangman/Hangman/HangmanViewController.swift
+++ Hangman/Hangman/HangmanViewController.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class HangmanViewController: UIViewController {
+    
+    @IBOutlet weak var hangManImage: UIImageView!
+    @IBOutlet weak var incorrectGuessesLabel: UILabel!
+    @IBOutlet weak var guessButton: UIButton!
+    
+    var currentGuessButton: UIButton!
+    var currentGuessChar: Character!
+    var incorrectGuesses = [String]()
+    
+    let alphabetString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,5 +45,30 @@ class HangmanViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    @IBAction func userDidPressLetter(_ sender: UIButton) {
+        hangManImage.image = #imageLiteral(resourceName: "hangman2")
+        currentGuessChar = getLetterFromIndex(sender.tag)
+        currentGuessButton = sender
+        if let guess = currentGuessChar {
+            guessButton.setTitle("Guess \(guess)", for: .normal)
+        }
+    }
 
+    @IBAction func userDidPressGuess(_ sender: UIButton) {
+        sender.setTitle("Guess", for: .normal)
+        if let letter = currentGuessChar {
+            incorrectGuesses.append(String(letter))
+            updateIncorrectGuessesUI()
+        }
+    }
+    
+    func getLetterFromIndex(_ pos: Int) -> Character {
+        return alphabetString[alphabetString.index(alphabetString.startIndex, offsetBy: pos)]
+    }
+    
+    func updateIncorrectGuessesUI() -> Void {
+        let formattedStr = incorrectGuesses.joined(separator: ", ")
+        incorrectGuessesLabel.text = "Incorrect Guesses: \(formattedStr)"
+        currentGuessButton.isEnabled = false
+    }
 }
